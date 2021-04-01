302cc17be466bddf142ae367f9be41da4231fb83
HeshanHH <heshanwijerathna01@gmail.com>
2021-04-01 15:25:41 +0530

    Add Models

---
+++ ChatApp/Models/Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+
+    public class Chat
+    {
+        public int Id{ get; set; }
+        public ICollection<Message> Messages{ get; set; }
+        public ICollection<User> Users{ get; set; }
+        public ChatType Type{ get; set; }
+        // prop for users
+    }
+}
---
+++ ChatApp/Models/ChatType.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Models
+{
+    public enum ChatType
+    {
+        Room,
+        Private,
+    };
+}
