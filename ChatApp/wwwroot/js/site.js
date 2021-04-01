575c391dd0921e4d11c3ffb48999ba0cb15f3f8d
HeshanHH <heshanwijerathna01@gmail.com>
2021-04-01 16:23:43 +0530

    Added JS and added modal controls

---
+++ ChatApp/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿var createRoomBtn = document.getElementById('create-room-btn');
+var createRoomModal = document.getElementById('create-room-modal');
+
+// add event listner to createRoomBtn.
+createRoomBtn.addEventListener('click', function () {
+    // set active.
+    createRoomModal.classList.add('active')
+});
+
+var closeModal = () => {
+    createRoomModal.classList.remove('active');
+};
